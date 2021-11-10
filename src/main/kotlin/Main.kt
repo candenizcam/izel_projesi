3d1c4c4b2e5d4115f165303816670f9dadf25c4c
candenizcam <e193777@metu.edu.tr>
2021-11-10 21:37:11 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    println("Hello World! başka bişy")
+
+    var mahmut = 12
+    var candan = 5
+    //// yukarısı input
+
+
+    // Try adding program arguments at Run/Debug configuration
+    println("Program arguments: ${mahmut/candan}")
+}
