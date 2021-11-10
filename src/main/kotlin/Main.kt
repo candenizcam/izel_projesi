80bf6aec78497c1f21e9cdc1730e7cc10b016db7
candenizcam <e193777@metu.edu.tr>
2021-11-10 22:28:18 +0300

    local chganes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,12 +2,22 @@ fun main(args: Array<String>) {
     println("Hello World! başka bişy")
 
     var mahmut = 12
-    var candan = 5
+    //var candan = 5
     //// yukarısı input
-    // mahmut 12 candan 5ken 2.4 çıkarıcaksın
+    // mahmut 3e bölünüyorsa hey, 5e bölünürse ho yazsın
 
-    var bölüm: Double = (mahmut).toDouble() / (candan).toDouble()
+    /*
+    if(bölüm>5.0){
+        println("if is true")
+
+    }else if(bölüm>10.0){
+        println("if is wrong")
+    }else{
+        println("hey hey hey")
+    }
+
+     */
 
     // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${bölüm}")
+    println("Program arguments: ${""}")
 }
